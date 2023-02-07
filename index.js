caf05e8c6eac31a6cb9b5b03bce3a7cc085c5200
Ujjwal Sharma <110032523+ujjwal5297@users.noreply.github.com>
2023-02-08 00:54:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,23 +11,24 @@ if(array[i] !== n){
     return newArray;
 }
 
-const result = deleteElement([10,20,30,50,40],50);
+const result = deleteElement([10,20,30,50,40],30);
 console.log(result)
 
 // Q2
 
 let myArr = [10,20,30,40,50,60];
-let item = 40;
+let item = 20;
 
 for(i=0; i<myArr.length-1; i++)
 {
     if(myArr[i] === item)
     {
-        index=i;
-        break; 
+        i;
+        break;
+         
     }
 }
-console.log(`Index no. of an element is ${index} and the element is ${item}`);
+console.log(`Index no. of an element is ${i} and the element is ${item}`);
 
 
 // Q3
